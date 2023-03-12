fa33076eee82bbd6feb71c5b47f368e3489e9a6b
Erik Kinnunen <erik.kinn@gmail.com>
2023-03-12 22:20:15 +0200

    scripts: add .gitconfig to symlink script
    Simplify symlink script by removing unnecessary variables and just
    reusing the word to make the paths more readable.

--- scripts/environment/symlink.sh
+++ scripts/environment/symlink.sh
@@ -1,39 +1,38 @@
-#!/bin/bash
-# =============================== #
-# Create soft links for dot files #
-# =============================== #
-BASE_PATH=$(git rev-parse --show-toplevel)
-CONFIG_PATH=$BASE_PATH/.config
-DESTINATION_CONFIG_PATH=$HOME/.config
-DESTINATION_BIN_PATH=/usr/local/bin
+#!/bin/sh
+
+readonly BASE_PATH="$(git rev-parse --show-toplevel)"
 
 # create nvim .config directory
-mkdir -p $DESTINATION_CONFIG_PATH/nvim
+mkdir -p "$HOME/.config/nvim"
 
 # .vimrc
-rm -f $HOME/.vimrc
-ln -s $BASE_PATH/.vimrc $HOME/.vimrc
+rm -f "$HOME/.vimrc"
+ln -s "$BASE_PATH/.vimrc" "$HOME/.vimrc"
 
 # .tmux.conf
-rm -f $HOME/.tmux.conf
-ln -s $BASE_PATH/.tmux.conf $HOME/.tmux.conf
+rm -f "$HOME/.tmux.conf"
+ln -s "$BASE_PATH/.tmux.conf" "$HOME/.tmux.conf"
 
 # .skhdrc
-rm -f $HOME/.skhdrc
-ln -s $BASE_PATH/.skhdrc $HOME/.skhdrc
+rm -f "$HOME/.skhdrc"
+ln -s "$BASE_PATH/.skhdrc" "$HOME/.skhdrc"
 
 # starship.toml
-rm -f $HOME/starship.toml
-ln -s $BASE_PATH/starship.toml $DESTINATION_CONFIG_PATH/starship.toml
+rm -f "$HOME/.config/starship.toml"
+ln -s "$BASE_PATH/starship.toml" "$HOME/.config/starship.toml"
 
 # alacritty.yml
-rm -f $HOME/.alacritty.yml
-ln -s $BASE_PATH/.alacritty.yml $HOME/.alacritty.yml
+rm -f "$HOME/.alacritty.yml"
+ln -s "$BASE_PATH/.alacritty.yml" "$HOME/.alacritty.yml"
 
 # yabairc
-rm -f $HOME/.yabairc
-ln -s $BASE_PATH/.yabairc $HOME/.yabairc
+rm -f "$HOME/.yabairc"
+ln -s "$BASE_PATH/.yabairc" "$HOME/.yabairc"
 
 # nvim
-rm -r $DESTINATION_CONFIG_PATH/nvim
-ln -s $CONFIG_PATH/nvim $DESTINATION_CONFIG_PATH/nvim
+rm -r "$HOME/.config/nvim"
+ln -s "$BASE_PATH/.config/nvim" "$HOME/.config/nvim"
+
+# gitconfig
+rm -r "$HOME/.gitconfig"
+ln -s "$BASE_PATH/.gitconfig" "$HOME/.gitconfig"
