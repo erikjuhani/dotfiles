799cd4b55c66968cf379345e874f94b695c621d4
Erik Kinnunen <erik.kinn@gmail.com>
2023-04-18 09:13:34 +0300

    Use lazy instead of packer
    Switching to lazy since it will allow more modular configuration more
    easily than packer.

--- .config/nvim/lua/local/plugins.lua
+++ .config/nvim/lua/local/plugins.lua
@@ -1,96 +1,117 @@
--- Automatically install packer
-local install_path = vim.fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = vim.fn.system {
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
     "git",
     "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]]
+local plugins = {
+  "nvim-lua/popup.nvim",
+  "nvim-lua/plenary.nvim",
+
+  -- cache
+  {
+    'lewis6991/impatient.nvim',
+    lazy = false,
+    priority = 1001,
+  },
+
+  -- git commands
+  "tpope/vim-fugitive",
+
+  -- style
+  { 
+    "sainnhe/sonokai",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- TODO: Re-organize plugin configurations to own folder config/<plugin>.lua
+      vim.cmd [[
+        filetype plugin on
+        syntax enable
+        set termguicolors
+        let $NVIM_TUI_ENABLE_TRUE_COLOR=1
+        set background=dark
+        let g:sonokai_better_performance=1
+        let g:sonokai_enable_italic=1
+        let g:sonokai_transparent_background=1
+        let g:sonokai_diagnostic_line_highlight=1
+        let g:sonokai_diagnostic_virtual_text='colored'
+        colorscheme sonokai
+      ]]
+
+      vim.cmd [[
+        hi NormalFloat guifg=NONE guibg=NONE
+        hi FloatBorder guibg=NONE
+        hi DiagnosticFloatingError guifg=#fc5d7c guibg=NONE
+        hi DiagnosticFloatingWarn guifg=#e7c664 guibg=NONE
+        hi DiagnosticFloatingInfo guifg=#76cce0 guibg=NONE
+      ]]
+
+      vim.cmd [[
+        hi Visual guifg=NONE guibg=#3f3f3f
+      ]]
+    end,
+  },
+
+  -- cmp plugins
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-buffer",
+  "hrsh7th/cmp-path",
+  "hrsh7th/cmp-cmdline",
+  "L3MON4D3/LuaSnip",
+  "ray-x/lsp_signature.nvim",
+
+  -- lsp
+  "neovim/nvim-lspconfig",
+  "jose-elias-alvarez/nvim-lsp-ts-utils",
+
+  -- telescope
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = 'make'
+  },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      'nvim-lua/popup.nvim',
+      'nvim-lua/plenary.nvim',
+      'telescope-fzf-native.nvim',
+      'nvim-telescope/telescope-ui-select.nvim',
+    },
+    cmd = 'Telescope',
+    build = "make",
+  },
+
+  -- treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+  },
+
+  -- Statusline
+  "itchyny/lightline.vim",
+
+  -- Motion
+  "chaoren/vim-wordmotion",
+  "FooSoft/vim-argwrap",
+
+  -- Language specifics
+  "ray-x/go.nvim",
+}
 
 -- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
+local ok, lazy = pcall(require, "lazy")
+if not ok then
   return
 end
 
--- Have packer use a popup window
-packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-  },
-}
-
-return packer.startup(function(use)
-  use "wbthomason/packer.nvim"
-  use "nvim-lua/popup.nvim"
-  use "nvim-lua/plenary.nvim"
-
-  -- cache
-  use 'lewis6991/impatient.nvim'
-
-  -- git commands
-  use "tpope/vim-fugitive"
-
-  -- style
-  use "sainnhe/sonokai"
-
-  -- cmp plugins
-  use "hrsh7th/nvim-cmp"
-  use "hrsh7th/cmp-nvim-lsp"
-  use "hrsh7th/cmp-buffer"
-  use "hrsh7th/cmp-path"
-  use "hrsh7th/cmp-cmdline"
-  use "L3MON4D3/LuaSnip" --snippet engine
-  use "ray-x/lsp_signature.nvim"
-
-  -- lsp
-  use "neovim/nvim-lspconfig"
-  use "jose-elias-alvarez/nvim-lsp-ts-utils"
-
-  -- Telescope
-  use "nvim-telescope/telescope.nvim"
-
-  use {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    run = "make",
-  }
-
-  -- Treesitter
-  use {
-    "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
-  }
-
-  -- Statusline
-  use {
-    "itchyny/lightline.vim"
-  }
-
-  -- Motion
-  use "chaoren/vim-wordmotion"
-  use "FooSoft/vim-argwrap"
-
-  -- Language specifics
-  use "ray-x/go.nvim"
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
-end)
+lazy.setup(plugins)
