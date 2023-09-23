5eac4c33a646e389b6ff462e20e0ae278e5b591e
Erik Kinnunen <erik.kinn@gmail.com>
2023-09-23 15:29:51 +0300

    nvim: efmls run formatting before write
    BufWritePost is handled after the file is written, thus resulting in
    saving at least twice to apply correct formatting and if using :wq, the
    formatting would never be written to the file. Changed to BufWritePre so
    formatting is ran _before_ saving the file.

--- .config/nvim/lua/local/efmls.lua
+++ .config/nvim/lua/local/efmls.lua
@@ -28,7 +28,7 @@ lspconfig.efm.setup {
 
 -- Format on save for efm
 local lsp_fmt_group = vim.api.nvim_create_augroup('LspFormattingGroup', {})
-vim.api.nvim_create_autocmd('BufWritePost', {
+vim.api.nvim_create_autocmd('BufWritePre', {
   group = lsp_fmt_group,
   callback = function()
     local efm = vim.lsp.get_active_clients({ name = 'efm' })
