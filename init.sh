c0c33e282829984d94a27c15403ae49f1e598781
Erik Kinnunen <erik.kinn@gmail.com>
2024-06-19 22:57:35 +0300

    Initialize `shm` properly with better prompt

--- init.sh
+++ init.sh
@@ -5,6 +5,10 @@ set -e
 # Download shm if it doesn't exist
 if ! command -v "shm" >/dev/null; then
   curl -sSL https://raw.githubusercontent.com/erikjuhani/shm/main/shm.sh | sh
+  if ! command -v "shm" >/dev/null; then
+    printf "%s" "Initialize PATH for shm, then run this script again"
+    exit 2
+  fi
 fi
 
 shm get erikjuhani/tm
