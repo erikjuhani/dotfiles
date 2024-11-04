30717cac2ee1bc16c23837fe2afe66f722dfad9c
Erik Kinnunen <erik.kinn@gmail.com>
2024-11-04 09:18:23 +0200

    Sketchybar: Use a nicer icon for battery plugin

--- .config/sketchybar/plugins/battery.sh
+++ .config/sketchybar/plugins/battery.sh
@@ -8,27 +8,22 @@ if [ "$PERCENTAGE" = "" ]; then
 fi
 
 case "${PERCENTAGE}" in
-  9[0-9] | 100) icon="󰁹" ;;
-  [8][0-9]) icon="󰂂" ;;
-  [7][0-9]) icon="󰂁" ;;
-  [6][0-9]) icon="󰂀" ;;
-  [5][0-9]) icon="󰁿" ;;
-  [4][0-9]) icon="󰁾" ;;
-  [3][0-9]) icon="󰁽" ;;
-  [2][0-9]) icon="󰁼" ;;
-  [1][0-9]) icon="󰁻" ;;
-  [0][0-9]) icon="󰁺" ;;
-  *) icon="󰂎" ;;
+  9[0-9] | 100) icon="" ;;
+  [6-8][0-9]) icon="" ;;
+  [3-5][0-9]) icon="" ;;
+  [1-2][0-9]) icon="" ;;
+  *) icon="" ;;
 esac
 
-if [[ "$CHARGING" != "" ]]; then
-  icon="󰂄"
-fi
+# if [[ "$CHARGING" != "" ]]; then
+#   icon=""
+#   icon_size="14.0"
+# fi
 
 # The item invoking this script (name $NAME) will get its icon and label
 # updated with the current battery status
 
 sketchybar --set "$NAME" icon="$icon" label="${PERCENTAGE}%" icon.drawing=on label.drawing=off \
-  label.font="BlexMono Nerd Font Mono:Regular:12.0" \
-  icon.font="BlexMono Nerd Font Mono:Regular:14.0" \
+  label.font="BlexMono Nerd Font Mono:Bold:12.0" \
+  icon.font="BlexMono Nerd Font Mono:Regular:${icon_size:-36.0}" \
   click_script="sketchybar --set $NAME icon.drawing=toggle label.drawing=toggle"
