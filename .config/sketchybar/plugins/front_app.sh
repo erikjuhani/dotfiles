7ba77a4d68bb9b1bb1bbd02734e3fde2855dadcd
Erik Kinnunen <erik.kinn@gmail.com>
2024-11-04 08:50:24 +0200

    Sketchybar: Use sketchybar-app-font to display app icons
    App icons are display according to the visible window. To enable this
    there exists a repository: https://github.com/kvndrsslr/sketchybar-app-font
    that generates Symbol font from application icon svgs and a bash script
    for easy access to the icons by only using the sketchybar INFO variable.
    
    Add font to the repository and cp the font to users fonts in symlink
    script. Unfortunately symlinking the font did not work.
    
    Use icon_map script in front_app plugin instead of custom mapping of
    application name to icons.

--- .config/sketchybar/plugins/front_app.sh
+++ .config/sketchybar/plugins/front_app.sh
@@ -5,15 +5,14 @@
 # focused application in the $INFO variable:
 # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
 
+source ./icon_map.sh
+
+__icon_map "${INFO}"
+icon="${icon_result}"
+
 if [ "$SENDER" = "front_app_switched" ]; then
-  case "${INFO}" in
-    Alacritty) icon="" ;;
-    Firefox) icon="󰈹" ;;
-    Obsidian) icon="" ;;
-    Slack) icon="" ;;
-    "System Settings") icon="" ;;
-  esac
-  sketchybar --set "$NAME" icon="${icon}" label="$INFO" \
-    icon.font="BlexMono Nerd Font Mono:Regular:20.0" \
+  sketchybar --set "$NAME" icon="${icon}" label="${INFO}" \
+    icon.font="${font:-sketchybar-app-font:Regular:14.0}" \
+    icon.y_offset=-1 \
     label.font="BlexMono Nerd Font Mono:Bold:12.0"
 fi
