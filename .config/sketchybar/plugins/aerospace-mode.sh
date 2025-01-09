f3f1fec2b8fd74a263c3f3cfac5c0e7da23288ae
Erik Kinnunen <erik.kinn@gmail.com>
2025-01-09 08:08:24 +0200

    Sketchybar: Remove Aerospace mode as obsolete

--- .config/sketchybar/plugins/aerospace-mode.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# The $NAME variable is passed from sketchybar and holds the name of
-# the item invoking this script:
-# https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
-
-sketchybar --set "$NAME" label="$(cat "${HOME}/.aerospace.mode")" label.color=0xffa7c080 \
-  label.font="BlexMono Nerd Font Mono:Light:14.0"
