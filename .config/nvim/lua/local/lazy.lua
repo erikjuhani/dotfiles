d2c9fe970f72632a1c091d250274120375d94c05
Erik Kinnunen <erik.kinn@gmail.com>
2025-02-26 09:19:52 +0200

    Add `kylechui/nvim-surround` to enable surround functionality
    Tool: nvim
    Plugin: nvim-surround

--- .config/nvim/lua/local/lazy.lua
+++ .config/nvim/lua/local/lazy.lua
@@ -149,6 +149,17 @@ local plugins = {
     end,
   },
 
+  {
+      "kylechui/nvim-surround",
+      version = "*", -- Use for stability; omit to use `main` branch for the latest features
+      event = "VeryLazy",
+      config = function()
+          require("nvim-surround").setup({
+              -- Configuration here, or leave empty to use defaults
+          })
+      end
+  },
+
   -- Motion
   { "chaoren/vim-wordmotion", lazy = false },
   { "FooSoft/vim-argwrap",    lazy = false },
