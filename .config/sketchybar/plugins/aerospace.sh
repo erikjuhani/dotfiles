78a3de375685dae2b9ae94ec8f5aacc871cbc8f6
Erik Kinnunen <erik.kinn@gmail.com>
2025-03-19 09:12:52 +0200

    Show all workspaces in sketchybar with details of open windows
    This required changing the aerospace configuration to send
    workspace_change events when workspace or window focus is changed.
    
    Changing focus also means opening or closing the windows.
    
    The aerospace.sh plugin was modifed to show tabs for each workspace.
    Initially the workspace number is shown. If one window is open in the
    workspace the windows' app icon and name is displayed. If there are
    multiple windows open in the single workspace each windows' app icon is
    displayed in the tab.
    
    Due to this change the currently opened window plugin was commented out
    since the information is now contained within the tab system.
    
    The currently focused workspace is shown with magenta highlight.

--- .config/sketchybar/plugins/aerospace.sh
+++ .config/sketchybar/plugins/aerospace.sh
@@ -7,10 +7,45 @@
 # sketchybar --set "$NAME" label="$(cat "${HOME}/.aerospace.mode")" label.color=0xffa7c080 \
 #   label.font="BlexMono Nerd Font Mono:Light:14.0"
 
-if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-  sketchybar --set "$NAME" background.drawing=on \
-    label.font="BlexMono Nerd Font Mono:Regular:12.0"
+source ./icon_map.sh
+
+current_workspace="$(aerospace list-workspaces --focused)"
+
+if [ "${current_workspace}" = "$1" ]; then
+  # Highlight the selected workspace
+  sketchybar --set "space.$1" background.color=0xff8334eb
 else
-  sketchybar --set "$NAME" background.drawing=off \
-    label.font="BlexMono Nerd Font Mono:Regular:12.0"
+  sketchybar --set "space.$1" background.color=0x22ffffff
+fi
+
+apps_in_workspace="$(aerospace list-windows --workspace "$1" | awk '{ print $3 }')"
+app_amount_in_workspace="$(aerospace list-windows --workspace "$1" | wc -l | awk '{$1=$1};1')"
+
+if [ "${app_amount_in_workspace}" -gt 1 ]; then
+  for app_name in $apps_in_workspace; do
+    __icon_map "${app_name}"
+    icon="${icon}${icon_result}"
+  done
+  sketchybar --set "space.$1" icon="${icon}" label.drawing=off \
+    icon.font="${font:-sketchybar-app-font:Regular:14.0}" \
+    icon.y_offset=-1 \
+    icon.padding_left=8 \
+    icon.padding_right=8 \
+    label.font="BlexMono Nerd Font Mono:Bold:12.0"
+elif [ -z "${apps_in_workspace}" ]; then
+  sketchybar --set "space.$1" icon.drawing=off label.drawing=on label="$1" \
+    label.padding_left=4 \
+    label.padding_right=4 \
+    label.font="BlexMono Nerd Font Mono:Bold:12.0"
+else
+  # Single app
+  __icon_map "${apps_in_workspace}"
+  icon="${icon_result}"
+
+  sketchybar --set "space.$1" icon="${icon}" label="${apps_in_workspace}" icon.drawing=on label.drawing=on \
+    icon.padding_left=8 \
+    icon.padding_right=0 \
+    label.padding_right=8 \
+    icon.font="${font:-sketchybar-app-font:Regular:14.0}" \
+    label.font="BlexMono Nerd Font Mono:Bold:13.0"
 fi
